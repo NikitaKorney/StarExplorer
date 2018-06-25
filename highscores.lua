12b5562dff85c71d808f1d27f4291608f9e157e2
Nikita Kornegrutsa <nikita_kornegrutsa@mail.ru>
2018-06-25 12:11:45 +0300

    исправили error

--- highscores.lua
+++ highscores.lua
@@ -62,38 +62,38 @@ function scene:create( event )
 	 table.insert(scoresTable, composer.getVariable("finalScore"));
 	 composer.setVariable("finalScore",0);
 
-end
 
-local function compare(a,b)
-	return a>b;
-end
 
-table.sort(scoresTable, compare);
-saveScores();
+	local function compare(a,b)
+		return a>b;
+	end
 
-	local background = display.newImageRect(sceneGroup, "photos/background.png",800,1400)
+	table.sort(scoresTable, compare);
+	saveScores();
+
+	local background = display.newImageRect( sceneGroup, "photos/background.png",800,1400)
 	background.x = display.contentCenterX;
 	background.y = display.contentCenterY;
 
 	local highScoresHeader = display.newText(sceneGroup, "High Scores ", display.contentCenterX,100,native.systemFont,44);
+	
 	for i = 1,10 do 
-		if(scoresTable[i]) then
+		if (scoresTable[i]) then
 			local yPos = 150 + (i*56);
 
-			local runkNum = display.newText(sceneGroup, i .. ")", display.contentCenterX-50,yPos,native.systemFont,36);
-			runkNum:setFillColor(0.8);
+			local rankNum = display.newText(sceneGroup, i .. ")", display.contentCenterX-50,yPos,native.systemFont,36);
+			rankNum:setFillColor(0.8);
 			rankNum.anchorX = 1;
 
 			local thisScore = display.newText(sceneGroup, scoresTable[i], display.contentCenterX-30,yPos,native.systemFont,36)
 			thisScore.anchorX = 0;
 			
 		end
-end
+	end
 
 	local menuButton  = display.newText(sceneGroup, "Menu ", display.contentCenterX,810,native.systemFont,44);
 	menuButton:setFillColor(0.75,0.78,1);
 	menuButton:addEventListener("tap",gotoMenu);
-
 end
 
 -- show()
